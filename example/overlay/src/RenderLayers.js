1290e637defeca7c43bd1ee8de6983bbd5a85b00
shimizu <xxshimizuxx@gmail.com>
2023-11-03 21:54:30 +0900

    コメント追加

--- example/overlay/src/RenderLayers.js
+++ example/overlay/src/RenderLayers.js
@@ -2,9 +2,6 @@ import { Tile3DLayer, GeoJsonLayer } from 'deck.gl';
 import { _TerrainExtension as TerrainExtension, FillStyleExtension } from '@deck.gl/extensions';
 import { Vector3 } from 'math.gl';
 
-const patterns = ['dots', 'hatch-1x', 'hatch-2x', 'hatch-cross'];
-const mapbox_tile_URL = "https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/512/{z}/{x}/{y}@2x?access_token=pk.eyJ1Ijoic2hpbWl6dSIsImEiOiJjbGZwbDg3YnUwYWV2M3FubDdvN3pqcDhxIn0.WfKF0oFWPFqS6xn0Mm0Yow"
-
 import { nest as d3nest } from 'd3-collection'
 import { color as d3color } from 'd3-color'
 import { zoning } from './zoningConf';
@@ -14,7 +11,7 @@ const zoningColor = d3nest()
   .map(zoning)
 
 
-
+//PLATEAUの 3D tileのURL
 const urls = [
   "https://storage.googleapis.com/g3-open-resource/PLATEAU/tokyo/13101_chiyoda-ku_notexture_2/13101_chiyoda-ku_notexture/tileset.json",
   "https://storage.googleapis.com/g3-open-resource/PLATEAU/tokyo/13102_chuo-ku_notexture_2/13102_chuo-ku_notexture/tileset.json",
@@ -25,7 +22,7 @@ const urls = [
 export function renderLayers({ visible }) {
   let layers = []
 
- 
+  //用途地域の塗り分けポリゴン
   const geoJsonLayer = new GeoJsonLayer({
     id: 'geojson-layer',
     data: "./data/area.geojson",
@@ -42,6 +39,7 @@ export function renderLayers({ visible }) {
 
   layers.push(geoJsonLayer);
 
+  //エクステンションで3Dタイルに適用するGeoJSON
   const overLayer = new GeoJsonLayer({
     id: 'over-layer',
     data: "./data/area.geojson",
@@ -54,6 +52,7 @@ export function renderLayers({ visible }) {
     },
     getLineColor: [0, 0, 0, 255],
     getLineWidth: 1,
+    //エクステンションを適用
     extensions: [new TerrainExtension()]
   });
 
@@ -61,6 +60,7 @@ export function renderLayers({ visible }) {
 
 
 
+  //PLATEAUの3D tileを描画
   urls.forEach((url,i)=>{
     const tile3Dlayer = new Tile3DLayer({
       id: 'tile-3d-layer' + i,
@@ -72,6 +72,7 @@ export function renderLayers({ visible }) {
           tileHeader.content.cartographicOrigin.z - 40,
         );
       },
+      //TerrainExtensionの適用対象に指定
       operation: 'terrain+draw'
     });
 
@@ -79,9 +80,6 @@ export function renderLayers({ visible }) {
 
   })
  
-
-
-
   //レイヤーの重なり順を配列で指定(先頭のレイヤーが一番下になる)
   return layers;
 }
